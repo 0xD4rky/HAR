44fc7830f5d715225c9cd8c6b26b2d622941db2c
Ishaan Kumar <ishaan.kumar281@gmail.com>
2024-08-24 15:30:31 +0530

    architecture added

--- 3dcnn/arch.py
+++ 3dcnn/arch.py
@@ -13,61 +13,81 @@ from tqdm import tqdm
 
 
 def conv3D_output_size(img_size, padding, kernel_size, stride):
-    # compute output shape of conv3D
+    
     outshape = (np.floor((img_size[0] + 2 * padding[0] - (kernel_size[0] - 1) - 1) / stride[0] + 1).astype(int),
                 np.floor((img_size[1] + 2 * padding[1] - (kernel_size[1] - 1) - 1) / stride[1] + 1).astype(int),
                 np.floor((img_size[2] + 2 * padding[2] - (kernel_size[2] - 1) - 1) / stride[2] + 1).astype(int))
     return outshape
 
-class CNN3D(nn.Module):
-    def __init__(self, t_dim=120, img_x=90, img_y=120, drop_p=0.2, fc_hidden1=256, fc_hidden2=128, num_classes=50):
-        super(CNN3D, self).__init__()
-
-        self.t_dim = t_dim
-        self.img_x = img_x
-        self.img_y = img_y
+class CNN3D(torch.nn.Module):
+    
+    def __init__(self, num_classes):
+        super().__init__()
+        self.conv1 = nn.Conv3d(5,64, kernel_size = 3, padding = 1)
+        self.bn1 = nn.BatchNorm3d(64)
+        self.pool1 = nn.MaxPool3d( kernel_size = 2, stride =  2)
         
-        self.fc_hidden1, self.fc_hidden2 = fc_hidden1, fc_hidden2
-        self.drop_p = drop_p
-        self.num_classes = num_classes
-        self.ch1, self.ch2 = 32, 48
-        self.k1, self.k2 = (5, 5, 5), (3, 3, 3)  # 3d kernel size
-        self.s1, self.s2 = (2, 2, 2), (2, 2, 2)  # 3d strides
-        self.pd1, self.pd2 = (0, 0, 0), (0, 0, 0)  # 3d padding
-
-        self.conv1_outshape = conv3D_output_size((self.t_dim, self.img_x, self.img_y), self.pd1, self.k1, self.s1)
-        self.conv2_outshape = conv3D_output_size(self.conv1_outshape, self.pd2, self.k2, self.s2)
-
-        self.conv1 = nn.Conv3d(in_channels=1, out_channels=self.ch1, kernel_size=self.k1, stride=self.s1,
-                               padding=self.pd1)
-        self.bn1 = nn.BatchNorm3d(self.ch1)
-        self.conv2 = nn.Conv3d(in_channels=self.ch1, out_channels=self.ch2, kernel_size=self.k2, stride=self.s2,
-                               padding=self.pd2)
-        self.bn2 = nn.BatchNorm3d(self.ch2)
-        self.relu = nn.ReLU(inplace=True)
-        self.drop = nn.Dropout3d(self.drop_p)
-        self.pool = nn.MaxPool3d(2)
-        self.fc1 = nn.Linear(self.ch2 * self.conv2_outshape[0] * self.conv2_outshape[1] * self.conv2_outshape[2],
-                             self.fc_hidden1)  # fully connected hidden layer
-        self.fc2 = nn.Linear(self.fc_hidden1, self.fc_hidden2)
-        self.fc3 = nn.Linear(self.fc_hidden2, self.num_classes)  # fully connected layer, output = multi-classes
-
-    def forward(self, x_3d):
-        # Conv 1
-        x = self.conv1(x_3d)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.drop(x)
-        # Conv 2
-        x = self.conv2(x)
-        x = self.bn2(x)
-        x = self.relu(x)
-        x = self.drop(x)
-        # FC 1 and 2
+        self.conv2 = nn.Conv3d(64, 128, kernel_size = 3, padding  = 1 )
+        self.bn2 = nn.BatchNorm3d(128)
+        self.pool2 = nn.MaxPool3d(kernel_size = 2, stride = 2)
+        
+        self.conv3a = nn.Conv3d(128,256, kernel_size = 3, padding = 1)
+        self.bn3a = nn.BatchNorm3d(256)
+        self.conv3b = nn.Conv3d(256, 256, kernel_size = 3, padding = 1)
+        self.bn3b = nn.BatchNorm3d(256)
+        self.pool3 = nn.MaxPool3d(kernel_size = 2, padding = 1)
+        
+        self.conv4a = nn.Conv3d(256,512, kernel_size = 3, padding = 1)
+        self.bn4a = nn.BatchNorm3d(512)
+        self.conv4b = nn.Conv3d(512, 512, kernel_size = 3, padding = 1)
+        self.bn4b = nn.BatchNorm3d(512)
+        self.pool4 = nn.MaxPool3d(kernel_size = 2, stride = 2)
+        
+        self.conv5a = torch.nn.Conv3d(512, 512, kernel_size=3, padding=1)
+        self.bn5a = torch.nn.BatchNorm3d(512)
+        self.conv5b = torch.nn.Conv3d(512, 512, kernel_size=3, padding=1)
+        self.bn5b = torch.nn.BatchNorm3d(512)
+        self.pool5 = torch.nn.MaxPool3d(kernel_size=2, stride=2)
+        
+        self.fc6 = torch.nn.Linear(512*1*7*7, 4096)
+        self.fc7 = torch.nn.Linear(4096,4096)
+        self.fc8 = torch.nn.Linear(4096,num_classes)
+        
+        self.dropout = torch.nn.Dropout(p = 0.5)
+        self.relu = torch.nn.ReLU()
+        
+    
+    def forward(self,x):
+        
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.pool1(x)
+        
+        x = self.relu(self.bn2(self.conv2(x)))
+        x = self.pool2(x)
+        
+        x = self.relu(self.bn3a(self.conv3a(x)))
+        x = self.relu(self.bn3b(self.conv3b(x)))
+        x = self.pool3(x)
+        
+        x = self.relu(self.bn4a(self.conv4a(x)))
+        x = self.relu(self.bn4b(self.conv4b(x)))
+        x = self.pool4(x)
+        
+        x = self.relu(self.bn5a(self.conv5a(x)))
+        x = self.relu(self.bn5b(self.conv5b(x)))
+        x = self.pool5(x)
+        
         x = x.view(x.size(0), -1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.dropout(x, p=self.drop_p, training=self.training)
-        x = self.fc3(x)
-
+        x = self.relu(self.fc6(x))
+        x = self.dropout(x)
+        x = self.relu(self.fc7(x))
+        x = self.relu(self.fc7(x))
+        x = self.dropout(x)
+        x = self.fc8(x)
+        
         return x
+    
+
+
+
+        
