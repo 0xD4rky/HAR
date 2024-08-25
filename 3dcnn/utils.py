defdac5d80b391575877fa072ba9b38a6292f06a
Ishaan Kumar <ishaan.kumar281@gmail.com>
2024-08-25 14:53:14 +0530

    json conversion

--- 3dcnn/utils.py
+++ 3dcnn/utils.py
@@ -19,3 +19,22 @@ def save_checkpoint(state, is_best, checkpoint_dir, best_model_dir):
         best_model_path = os.path.join(best_model_dir, 'best_model.pth')
         torch.save(state['state_dict'], best_model_path)
         
+def load_checkpoint(checkpoint_path, model, optimizer = None):
+    """
+    Load model checkpoint
+    """
+    if not os.path.exists(checkpoint_path):
+        raise FileNotFoundError(f"File doesn't exist {checkpoint_path}")
+    checkpoint = torch.load(checkpoint_path)
+    model.load_state_dict(checkpoint['state_dict'])
+    if optimizer:
+        optimizer.load_state_dict(checkpoint['optimizer'])
+    return checkpoint['epoch'], checkpoint['best_acc']
+
+def save_dict_to_json(d, json_path):
+    """
+    Save dict of floats to json file
+    """
+    with open(json_path, 'w') as f:
+        d = {k: float(v) for k, v in d.items()}
+        json.dump(d, f, indent=4)
